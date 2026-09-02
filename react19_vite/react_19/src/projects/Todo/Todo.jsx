f4e1509ab64813d784ce4307a69ce81d0d614543
hamza <nomanhamza074@gmail.com>
2026-09-02 16:43:02 +0500

    use-state-added-todo

--- react19_vite/react_19/src/projects/Todo/Todo.jsx
+++ react19_vite/react_19/src/projects/Todo/Todo.jsx
@@ -1,6 +1,10 @@
 import "./Todo.css";
+import { useState } from "react";
 
 export const Todo = () => {
+
+    const [inputValue, setInputValue] = useState("");
+
     return (
         <section className="todo-container">
             <header>
@@ -8,7 +12,7 @@ export const Todo = () => {
             </header>
             <section className="form">
                 <form>
-                    <input type="text" className="todo-input" autoComplete="off" />
+                    <input type="text" className="todo-input" autoComplete="off" value={inputValue} />
                     <button type="submit" className="todo-btn">
                         Add Task
                     </button>
